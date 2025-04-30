732e160b77ce15acebe802c437b4b3b63fb4e283
StefanMaring <smaring@ziggo.nl>
2025-04-30 23:55:37 +0200

    Recently played styling

--- src/components/RecentlyPlayed.vue
+++ src/components/RecentlyPlayed.vue
@@ -3,13 +3,43 @@
         <div class="content">
             <h2>Recently played:</h2>
             <div class="recently-played-list">
-                
+                <div class="recently-played-item" v-for="(song, index) in recentlyPlayed" :key="index">
+                    <div class="recently-played-info">
+                        <p>{{ song.timestamp }}</p>
+                        <i>{{ song.filename }}</i>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
 </template>
 <script>
+import { radioService } from '@/services/radioService'
+
 export default {
     name: 'RecentlyPlayed',
+    data() {
+        return {
+            recentlyPlayed: [],
+        }
+    },
+    methods: {
+        async loadRecentlyPlayed() {
+            try {
+                const response = await radioService.getRecentlyPlayed("/recentlyPlayed");
+                if (response.ok) {
+                    const data = await response.json();
+                    this.recentlyPlayed = data.data.reverse();
+                } else {
+                    toastr.error("Failed to send recently played song.", "Error");
+                }
+            } catch (error) {
+                toastr.error("Error sending recently played song:", "Error");
+            }
+        },
+    },
+    mounted() {
+        this.loadRecentlyPlayed()
+    },
 }
 </script>
