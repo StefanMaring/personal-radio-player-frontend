1686485a6ddbc63cf1ebc08d356fbcf980e5359a
StefanMaring <smaring@ziggo.nl>
2025-09-27 22:58:23 +0200

    Song info changes and fixes
    Changed the display behaviour of the different info types

--- src/components/SongInfo.vue
+++ src/components/SongInfo.vue
@@ -2,12 +2,12 @@
     <section class="s-song-info">
         <div class="content">
             <h2>Song Info:</h2>
-            <div v-if="recording" class="song-details">
+            <div class="song-details">
                 <div><span>Title:</span><span>{{ songInfo.title }}</span></div>
                 <div><span>Artist:</span><span>{{ songInfo.artist }}</span></div>
                 <div><span>Album:</span><span>{{ songInfo.album }}</span></div>
-                <div v-if="songInfo.releaseDate"><span>Released:</span><span>{{ songInfo.releaseDate }}</span></div>
-                <div v-if="songInfo.genre"><span>Genre:</span><span>{{ songInfo.genre }}</span></div>
+                <div><span>Released:</span><span>{{ songInfo.releaseDate }}</span></div>
+                <div><span>Genre:</span><span>{{ songInfo.genre }}</span></div>
             </div>
         </div>
     </section>
@@ -42,13 +42,20 @@ export default {
                 if(data.recordings.length > 0) {
                     let recordings = data.recordings;
 
-                    recordings.forEach((recording) => {
-                        if(recording.title === currentSongSplit[1].trim() && recording['artist-credit'][0].name === currentSongSplit[0].trim()) {
+                    for (let i = 0; i < recordings.length; i++) {
+                        const recording = recordings[i];
+                        
+                        if (
+                            recording.title.trim().toLowerCase() === currentSongSplit[1].trim().toLowerCase() &&
+                            recording['artist-credit'][0].name.trim().toLowerCase() === currentSongSplit[0].trim().toLowerCase()
+                        ) {
                             this.recording = recording;
                             this.setSongInfo(recording);
-                            return; 
+                            break;
+                        } else {
+                            this.clearSongInfo();
                         }
-                    });
+                    }
                 }
             } else {
                 console.log("Error fetching song info");
@@ -59,11 +66,39 @@ export default {
             this.songInfo.title = recording.title;
             this.songInfo.artist = recording['artist-credit'][0].name;
             this.songInfo.album = recording.releases[0].title;
-            this.songInfo.releaseDate = recording['first-release-date'];
+
+            if(recording['first-release-date']) {
+                this.songInfo.releaseDate = recording['first-release-date'];
+            } else {
+                this.songInfo.releaseDate = "-";
+            }
 
             if(recording.tags && recording.tags.length > 0) {
-                this.songInfo.genre = recording.tags[0].name;
+                //Make sure the genre names start with a capitalized letter, since the API doesn't deliver them that way
+                recording.tags.map(tag => tag.name = tag.name.charAt(0).toUpperCase() + tag.name.slice(1));
+
+                let output = "";
+
+                //Allow for up to two genre tags to be shown
+                if(recording.tags[1] !== "") {
+                    output += recording.tags[0].name + "/" + recording.tags[1].name;
+                } else {
+                    output += recording.tags[0].name;
+                }
+
+                this.songInfo.genre = output;
+            } else {
+                this.songInfo.genre = "-";
             }
+        },
+
+        clearSongInfo() {
+            this.recording = "";
+            this.songInfo.title = "-";
+            this.songInfo.artist = "-";
+            this.songInfo.album = "-";
+            this.songInfo.releaseDate = "-";
+            this.songInfo.genre = "-";
         }
     }
 }
